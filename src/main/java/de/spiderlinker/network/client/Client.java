fec862a986bee386ac675dcaf58a62968799535b
Spiderlinker <support@spiderlinker.de>
2018-03-31 09:22:28 +0200

    Moved to different package and improved code

---
+++ src/main/java/de/spiderlinker/network/client/Client.java
@@ -0,0 +1,172 @@
+package de.spiderlinker.network.client;
+
+import de.spiderlinker.network.data.DataPackage;
+import de.spiderlinker.network.utils.ConnectionUtils;
+import de.spiderlinker.network.utils.SocketUtils;
+import de.spiderlinker.utils.StringUtils;
+
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Client {
+
+  private String    host;
+  private int       port;
+  private int       timeout;
+  private SSLSocket activeConnection;
+
+  public Client() {
+    // create empty clientSocket with no information about host or port
+  }
+
+  public Client(final String host, final int port) {
+    this(host, port, SocketUtils.DEFAULT_TIMEOUT);
+  }
+
+  public Client(final String host, final int port, int timeout) {
+    this.updateConnection(host, port, timeout);
+  }
+
+  /**
+   * Updates host and port to the passed values
+   *
+   * @param host new host
+   * @param port new port
+   */
+  public void updateConnection(final String host, final int port, int timeout) {
+    this.updateHost(host);
+    this.updatePort(port);
+    this.updateTimeout(timeout);
+  }
+
+  /**
+   * Updates the host to the passed value
+   *
+   * @param host new host to be set
+   */
+  public void updateHost(final String host) {
+    this.host = StringUtils.requireNonNullOrEmpty(host);
+  }
+
+  /**
+   * Updates the port to the passed value
+   *
+   * @param port new port to be set
+   */
+  public void updatePort(final int port) {
+    this.port = SocketUtils.checkPort(port);
+  }
+
+  /**
+   * Updates the timeout to the host
+   *
+   * @param timeout timeout to be set
+   */
+  public void updateTimeout(int timeout) {
+    this.timeout = SocketUtils.checkTimeout(timeout);
+  }
+
+  /**
+   * @return Connected host of this clientSocket
+   */
+  public String getHost() {
+    return host;
+  }
+
+  /**
+   * @return port of connected host
+   */
+  public int getPort() {
+    return port;
+  }
+
+  /**
+   * @return timeout of connected host
+   */
+  public int getTimeout() {
+    return timeout;
+  }
+
+  /**
+   * Sends all passed data with the given id to the specified host (and port)
+   *
+   * @param id   id of data package
+   * @param data data to be sent
+   * @throws IOException host not reachable or other connect exception
+   */
+  public void sendMessage(final String id, final Object... data)
+      throws IOException {
+    this.sendMessage(new DataPackage(id, data));
+  }
+
+  /**
+   * Sends all passed data with the given id to the specified host (and port)
+   *
+   * @param data data to be sent
+   * @throws IOException host not reachable or other connect exception
+   */
+  public void sendMessage(final DataPackage data) throws IOException {
+    establishConnection();
+    ConnectionUtils.performHandshake(activeConnection);
+    ConnectionUtils.writeObject(activeConnection, data);
+  }
+
+  /**
+   * Receives data from established connection (with given host & port)
+   *
+   * @return received data from host
+   * @throws IOException host not reachable or other network error
+   */
+  public DataPackage receiveMessage() throws IOException {
+    establishConnection();
+    ConnectionUtils.handleHandshake(activeConnection);
+    return receiveData();
+  }
+
+  private DataPackage receiveData() throws IOException {
+    DataPackage data = null;
+    try {
+      Object rawData = ConnectionUtils.readObject(activeConnection);
+      if (rawData instanceof DataPackage) {
+        data = (DataPackage) rawData;
+      }
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    return data;
+  }
+
+  private void establishConnection() throws IOException {
+    if (isConnectionClosed()) {
+      activeConnection = SocketUtils.createSSLSocket(host, port, timeout);
+    }
+  }
+
+  private boolean isConnectionClosed() {
+    return activeConnection == null || activeConnection.isClosed();
+  }
+
+  /**
+   * Closes the active connection (if there is any)
+   */
+  public void closeConnection() {
+    ConnectionUtils.close(activeConnection);
+  }
+
+  /**
+   * Checks if this clientSocket can connect to the specified host
+   *
+   * @return whether host is reachable or not
+   */
+  public boolean isReachable() {
+    try {
+      return InetAddress.getByName(host).isReachable(SocketUtils.DEFAULT_TIMEOUT);
+    } catch (IOException e) {
+      // TODO log
+      return false;
+    }
+  }
+
+
+}
---
+++ src/main/java/de/spiderlinker/network/server/ServerManager.java
@@ -0,0 +1,235 @@
+package de.spiderlinker.network.server;
+
+import de.spiderlinker.network.data.DataPackage;
+import de.spiderlinker.network.data.Executable;
+import de.spiderlinker.network.utils.ConnectionUtils;
+import de.spiderlinker.network.utils.SocketUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ServerManager {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ServerManager.class);
+
+  private final Map<String, Executable> registeredMethods = new HashMap<>();
+
+  private SSLServerSocket serverSocket;
+  private int             serverListeningPort;
+  private int             clientLimit;
+  private Thread          serverHandleThread;
+  private boolean         isServerAlive;
+
+  public ServerManager(final int port) {
+    this(port, SocketUtils.DEFAULT_LIMIT);
+  }
+
+  public ServerManager(final int port, final int clientLimit) {
+    this.setPort(port);
+    this.setClientLimit(clientLimit);
+    this.initialize();
+  }
+
+  private void setPort(final int port) {
+    this.serverListeningPort = SocketUtils.checkPort(port);
+  }
+
+  private void setClientLimit(int limit) {
+    this.clientLimit = limit;
+  }
+
+  /**
+   * This method is called when the server is getting initialized <br>
+   * Put code in here to run it before the server starts (Nothing in here yet)
+   */
+  protected void initialize() {
+  }
+
+  /**
+   * Register the executable to the id. Every incoming data with
+   * the specified id will be handed over to the passed executable.
+   *
+   * @param id   id of data to claim
+   * @param exec executable to be executed when data with specified id will arrive
+   */
+  public void registerMethod(final String id, final Executable exec) {
+    this.registeredMethods.put(id, exec);
+  }
+
+  /**
+   * This method will be called if there was no suitable method was registered for the received data.
+   * This method is empty by default.
+   *
+   * @param data   DataPackage that could not be assigned to a registered method
+   * @param socket client that sent this data
+   */
+  protected void onUnidentifiedMessage(final DataPackage data, final Socket socket) {
+    LOGGER.warn("No implementation for handling of unidentified messages!\nUnidentified data '{}' from '{}'", data, socket);
+  }
+
+  /**
+   * Send given data to specified client. The specified client and data must not be null!
+   * The client must be able to perform a handshake and reading the data!
+   *
+   * @param host address of client
+   * @param port port of client
+   * @param msg  message to send
+   * @throws IOException failed to send message
+   */
+  public void sendMessage(final String host, final int port, final DataPackage msg)
+      throws IOException {
+    this.sendMessage(SocketUtils.createSSLSocket(host, port), msg);
+  }
+
+  /**
+   * Send given data to specified client. The specified client and data must not be null!
+   * The client must be able to perform a handshake and reading the data!
+   *
+   * @param socket address of client / recipient
+   * @param msg    message to send
+   * @throws IOException failed to send message
+   */
+  public void sendMessage(final SSLSocket socket, final DataPackage msg) throws IOException {
+    if (socket == null || msg == null) {
+      // do not send data
+      return;
+    }
+
+    try {
+      ConnectionUtils.performHandshake(socket);
+      ConnectionUtils.writeObject(socket, msg);
+    } catch (final IOException e) {
+      LOGGER.error("Error while sending message to client!", e);
+      throw e;
+    }
+  }
+
+  private void createServerListeningThread() {
+    this.serverHandleThread = new Thread(() -> {
+      try {
+        createServerSocket();
+        waitForIncomingConnections();
+      } catch (final IOException e) {
+        LOGGER.error("Error while starting server", e);
+      }
+    });
+  }
+
+  private void createServerSocket() throws IOException {
+    LOGGER.info("Creating new server listening socket on {} with limit {}", serverListeningPort, clientLimit);
+    this.serverSocket = SocketUtils.createSSLServerSocket(this.serverListeningPort, this.clientLimit);
+  }
+
+  private void waitForIncomingConnections() {
+    LOGGER.info("Start listening... >> {}", serverSocket);
+    while (ServerManager.this.isServerAlive) {
+      LOGGER.info("Waiting for clientSocket...");
+      handleIncomingClientConnectionInNewThread();
+    }
+  }
+
+  private void handleIncomingClientConnectionInNewThread() {
+    try {
+      SSLSocket client = (SSLSocket) this.serverSocket.accept();
+      Runnable clientHandle = createConnectionHandle(client);
+      new Thread(clientHandle).start();
+    } catch (final IOException e) {
+      // If the server was stopped the #accept() method will throw this error
+      // because the socket was closed. It is no error (only if server is alive)
+      if (isServerAlive) {
+        LOGGER.error("Error while accepting clientSocket", e);
+      }
+    }
+  }
+
+  private Runnable createConnectionHandle(final SSLSocket socket) {
+    return () -> {
+      try {
+        if (!ConnectionUtils.handleHandshake(socket)) {
+          return;
+        }
+
+        LOGGER.info("Handle connection: {}", socket);
+        final Object raw = ConnectionUtils.readObject(socket);
+
+        LOGGER.info("Incoming data (raw): {}", raw);
+        processReceivedRawData(raw, socket);
+
+      } catch (IOException | ClassNotFoundException e) {
+        LOGGER.error("Error while handling clientSocket connection", e);
+      }
+    };
+  }
+
+  private void processReceivedRawData(Object rawData, SSLSocket client) {
+    if (!isDataPackage(rawData)) {
+      return;
+    }
+
+    final DataPackage data = (DataPackage) rawData;
+    Executable dataHandle = getExecutableForReceivedData(data);
+
+    LOGGER.info("Method for incoming data with id '{}' found: {} (from address: {})",
+        data.getID(), dataHandle != null /* TODO remove this unnecessary output*/, client);
+
+    runExecutableOrDefaultInThread(dataHandle, data, client);
+  }
+
+  private boolean isDataPackage(Object rawData) {
+    return rawData instanceof DataPackage;
+  }
+
+  private Executable getExecutableForReceivedData(DataPackage data) {
+    return registeredMethods.get(data.getID());
+  }
+
+  private void runExecutableOrDefaultInThread(Executable executable, DataPackage data, SSLSocket client) {
+    new Thread(executable == null // TODO in another thread? There was already one started
+        ? () -> this.onUnidentifiedMessage(data, client) //
+        : () -> executable.run(data, client) //
+    ).start();
+  }
+
+  /**
+   * Starts the server if it is not already started
+   *
+   * @return if the server was successfully started
+   */
+  public boolean start() {
+    boolean started = false;
+
+    LOGGER.info("Starting server (existing socket: {}) at port {}", serverSocket, serverListeningPort);
+
+    if (this.serverSocket == null && !this.isServerAlive) {
+      this.createServerListeningThread();
+      this.isServerAlive = true;
+      this.serverHandleThread.start();
+      started = true;
+    }
+    /* otherwise the server is already started */
+
+    return started;
+  }
+
+  /**
+   * Stop the server => stop listening thread and close server socket
+   */
+  public void stop() {
+    LOGGER.info("Stopping server {}", serverSocket);
+
+    if (this.serverHandleThread != null) {
+      this.serverHandleThread.interrupt();
+    }
+
+    this.isServerAlive = false;
+    ConnectionUtils.close(this.serverSocket);
+    this.serverSocket = null;
+  }
+
+}
